7eb9a1753b3f5c190ec0bacc7bf2070404dff7ca
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2023-03-10 14:53:42 +0900

    basic example

--- basic_example.py
+++ basic_example.py
@@ -1,1 +1,11 @@
+import openai
 
+openai.api_key = key
+
+# basic example 1
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt="who was Carl sagan?"  # query
+)
+print(response)  # print all response
+print(response.choices[0].text)
